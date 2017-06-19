704722717554a4f822a6f89d663d91d036b74633
speronem <msperone@usnews.com>
2017-06-19 15:22:44 -0400

    new vim things, update update script

--- update.sh
+++ update.sh
@@ -2,20 +2,29 @@
 # update DefaultConfigFiles to this system's current files
 echo "-- Update Config Files --"
 echo " "
-echo "Backing up config files to f:/DefaultConfigFiles"
-echo "Press [Y|n] to continue >"
-read continue_update
-if [[ $continue_update == "y" || $continue_update == "Y" ]]; then
-    echo "..."
-    backup_folder="/mnt/f/DefaultConfigFiles"
-    echo "vim files..."
-    cp ~/.vimrc "${backup_folder}/vim/.vimrc"
-    cp -r ~/.vim/colors/ "${backup_folder}/vim/.vim/colors"
-    echo "bash aliases..."
-    cp ~/.bash_aliases "${backup_folder}/bash/.bash_aliases"
-else
-    echo "You selected no, good day"
-fi
+backup_folder="/mnt/f/DefaultConfigFiles"
+
+confirm_backup ()
+{
+	echo "Backing up config files to ${backup_folder}"
+	echo "Press [Y|n] to continue >"
+	read continue_update
+
+	if [[ $continue_update == "y" || $continue_update == "Y" ]]; then
+		echo "..."
+	else
+		echo "Enter the directory you wouldlike to backup to >"
+		read backup_folder
+		confirm_backup
+	fi
+}
+confirm_backup
+
+echo "vim files..."
+cp ~/.vimrc "${backup_folder}/vim/.vimrc"
+cp -r ~/.vim/colors/ "${backup_folder}/vim/.vim/colors"
+echo "bash aliases..."
+cp ~/.bash_aliases "${backup_folder}/bash/.bash_aliases"
 
 echo "All done, thank you"
 
--- vim/.vimrc
+++ vim/.vimrc
@@ -10,9 +10,21 @@ colorscheme molokai
 set background=dark
 
 "  Spaces and Tabs
+filetype plugin indent on
 set tabstop=4
 set softtabstop=4
 set expandtab
+set autoindent
+
+" Directory (netrw)
+"  1: wide (file info)
+"  3: tree
+
+" Landscape View Setup
+"
+let g:netrw_liststyle = 1 
+let g:netrw_browse_split = 4
+let g:netrw_altv = 1
 
 " Searching
 set hlsearch
