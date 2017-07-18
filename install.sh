8a9be84bc60a10be4eac0b7878c3b2cbf508e318
Mike Sperone (from my MBP) <mike@mikesperone.com>
2017-07-18 10:50:41 -0400

    install from current directory

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 # update DefaultConfigFiles to this system's current files
 echo "-- Install Config Files --"
 echo " "
-echo "Install config files from f:/DefaultConfigFiles"
+echo "Install config files from this directory"
 echo "ARE YOU SURE!! THIS WILL OVERWRITE YOUR CURRENT .vimrc AND .bash_aliases FILES!"
 echo "Press [Y|n] to continue >"
 read continue_update
 if [[ $continue_update == "y" || $continue_update == "Y" ]]; then
     echo "..."
-    backup_folder="/mnt/f/DefaultConfigFiles"
+    backup_folder="."
     echo "vim files..."
     cp "${backup_folder}/vim/.vimrc" ~/.vimrc
     cp -r "${backup_folder}/vim/colors" ~/.vim/colors
