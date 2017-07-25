ab0063fe7fbe3518879560388d089619fc3faf45
Mike Sperone <mike@mikesperone.com>
2017-07-25 13:19:19 -0400

    install syntastic

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ if [[ $continue_update == "y" || $continue_update == "Y" ]]; then
     mkdir -p ~/.vim/autoload ~/.vim/bundle && \
             curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
     cd ~/.vim/bundle
+    git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
     git clone git://github.com/airblade/vim-gitgutter.git
     echo "--- BASH ---"
     echo "aliases..."
