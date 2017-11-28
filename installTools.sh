1fb8bcb13c3eb1a1f485943e8fb785ec1628ce29
Mike Sperone <mike@mikesperone.com>
2017-11-28 11:11:48 -0500

    Update installTools.sh

--- installTools.sh
+++ installTools.sh
@@ -1,7 +1,8 @@
-" 
-"  Tools I like to use
-"   This will install all of them
+#!/bin/bash
+#
+# Tools I like to use
+#   This will install all of them
 
-" terminal markdown viewer
+# terminal markdown viewer
 npm i -g msee
 
