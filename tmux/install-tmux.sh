74adacacad23439b8b760da84b4e71025219b7c1
Michael Sperone <msperone@usnews.com>
2019-11-22 11:19:27 -0600

    add tmux-powerline conf

--- tmux/install-tmux.sh
+++ tmux/install-tmux.sh
@@ -4,4 +4,5 @@ cp $HOME/.tmux.conf $HOME/.tmux.conf.bak
 echo "updating your tmux conf"
 cp ./.tmux.conf $HOME/.tmux.conf
 
-echo "now update the director value in your $HOME/.tmux.conf"
+echo "copying tmux-powerline conf"
+cp ./.tmux-powerlinerc $HOME/.tmux-powerlinerc
