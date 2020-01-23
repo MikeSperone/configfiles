69d72620fe8a0621d6584318b967235fe021fee2
Michael Sperone <msperone@usnews.com>
2020-01-23 11:49:54 -0600

    include todo in installTools, separate detect_os script

--- installTools.sh
+++ installTools.sh
@@ -3,7 +3,16 @@
 # Tools I like to use
 #   This will install all of them
 
-# msee | terminal markdown viewer
-# fx   | json viewer
+source ./utils/detect_os.sh
+
+echo "msee | terminal markdown viewer"
+echo "fx   | json viewer"
 npm i -g msee fx
+
+echo "todo | simple task list"
+if [[ "$platform" == 'mac' ]]; then
+    brew install todo-txt
+else
+    echo "I did not set this up for linux yet."
+fi
 
